c09ebc56ebbf12e1c2ea5557d2c1b4fe383ce674
cskitty <coolcottontail@gmail.com>
2020-07-03 11:24:45 -0700

    fixed QSP1, QSP2, QSP5

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ module.exports = async function(deployer) {
   await deployer.deploy(BUSD);
   const proxy = await deployer.deploy(Proxy, BUSD.address);
   const proxiedBUSD = await BUSD.at(proxy.address);
-  await proxy.changeAdmin("0xf0b1eef88956b0a307fa87b5f5671aad6a5d330f");
+  await proxy.changeAdmin("0x0e9d874fb23614f3f0c16fcc5802cb5e967b58f3");
   await proxiedBUSD.initialize();
   await proxiedBUSD.initializeDomainSeparator();
 };
